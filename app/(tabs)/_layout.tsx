1c691e93bb4a31eed7b8f51ac524f68fbb35ee74
thisaramax20 <thisarathejana20@gmail.com>
2025-05-01 08:22:55 +0530

    add customized bottom navigation bar

---
+++ app/(tabs)/_layout.tsx
@@ -0,0 +1,76 @@
+import React from "react";
+import { Tabs } from "expo-router";
+import { Image, ImageBackground, Text } from "react-native";
+import { images } from "@/constants/images";
+import { icons } from "@/constants/icons";
+import TabIcon from "@/components/TabIcon";
+
+const _layout = () => {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarShowLabel: false,
+        tabBarItemStyle: {
+          width: "100%",
+          height: "100%",
+          justifyContent: "center",
+          alignItems: "center",
+        },
+        tabBarStyle: {
+          backgroundColor: "#0f0D23",
+          borderRadius: 50,
+          marginHorizontal: 20,
+          marginBottom: 36,
+          height: 52,
+          position: "absolute",
+          overflow: "hidden",
+          borderWidth: 1,
+          borderColor: "0f0d23",
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.home} title={"Home"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.person} title={"Profile"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Saved"
+        options={{
+          title: "Saved",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.save} title={"Saved"} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="Search"
+        options={{
+          title: "Search",
+          headerShown: false,
+          tabBarIcon: ({ focused }) => (
+            <TabIcon focused={focused} icon={icons.search} title={"Search"} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+};
+
+export default _layout;
---
+++ components/TabIcon.tsx
@@ -0,0 +1,26 @@
+import { Text, ImageBackground, Image, View } from "react-native";
+import React from "react";
+import { images } from "@/constants/images";
+
+const TabIcon = ({ focused, icon, title }) => {
+  if (focused) {
+    return (
+      <ImageBackground
+        source={images.highlight}
+        className="flex flex-row w-full flex-1 min-w-[112px] min-h-14 mt-4 justify-center items-center rounded-full overflow-hidden"
+      >
+        <Image source={icon} tintColor="#151312" className="size-5" />
+        <Text className="text-secondary text-base font-semibold ml-2">
+          {title}
+        </Text>
+      </ImageBackground>
+    );
+  }
+  return (
+    <View className="size-full justify-center items-center mt-4 rounded-full">
+      <Image source={icon} tintColor="#A8B5DB" className="size-5" />
+    </View>
+  );
+};
+
+export default TabIcon;
